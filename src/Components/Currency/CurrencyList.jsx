6a283455f6b37d62e6c85e954380977584f68ec3
Divanshu Kachhawa <divanshukachawa7878@gmail.com>
2024-01-17 18:42:11 +0530

    first commit1

--- src/Components/Currency/CurrencyList.jsx
+++ src/Components/Currency/CurrencyList.jsx
@@ -19,14 +19,14 @@ const CurrencyConverter = () => {
   } = useHooksCurrencey();
 
   return (
-    <div className="border rounded w-[300px] h-[450px] flex flex-col flex-wrap py-2 px-2 bg-white " id="sahdow">
+    <div className="border rounded w-[300px] h-[450px] lg:w-[500px] flex flex-col flex-wrap py-2 px-2 bg-white " id="sahdow">
       <section className="w-full h-[90%] flex flex-col justify-center px-2 gap-6">
       <div className="w-full h-[10%] flex flex-col justify-center">
-      <h1 className="text-center text-2xl font-bold bg-clip-text text-transparent" id="back-2">Currency Converter</h1>
+      <h1 className="text-center text-2xl font-bold bg-clip-text text-transparent lg:text-3xl" id="back-2">Currency Converter</h1>
       </div>
       <FontAwesomeIcon icon={faCoins} className="text-7xl text-yellow-400"/>
         <div className="flex flex-col">
-          <label className="text-lg font-semibold">Enter {fromCurrency} Amount</label>
+          <label className="text-lg font-semibold lg:text-xl">Enter {fromCurrency} Amount</label>
           <div className="shadow-md shadow-teal-300 border-black border rounded" >
             <input
             type="number" 
@@ -37,16 +37,16 @@ const CurrencyConverter = () => {
 
         <div className="w-full flex justify-between items-center">
           <div>
-            <label className="text-lg font-semibold">From</label>
+            <label className="text-lg font-semibold lg:text-xl" >From</label>
             <CurrencyOptions
               currenciesdiraction={fromCurrency}
               setDiraction={setFromCurrency}
               currencies={currencies}
             />
           </div>
-          <button onClick={() => swapCurrency()}><FontAwesomeIcon icon={faRightLeft} className="mt-9"/></button>
+          <button onClick={() => swapCurrency()}><FontAwesomeIcon icon={faRightLeft} className="mt-9 text-lg"/></button>
           <div>
-            <label className="text-lg font-semibold">To</label>
+            <label className="text-lg font-semibold lg:text-xl">To</label>
             <CurrencyOptions
               currenciesdiraction={toCurrency}
               setDiraction={setToCurrency}
@@ -55,10 +55,10 @@ const CurrencyConverter = () => {
           </div>
         </div>
 
-        {amount.length === 0 ? <div className="text-center text-lg font-semibold text-[#e50914]">Please Enter any Amount!</div>:isloading? <div className="flex justify-center"><Loader/></div>:<div className="flex flex-col text-lg font-semibold gap-1">
-          <p className="text-center">Converted Amount:</p> 
+        {amount.length === 0 ? <div className="text-center text-lg font-semibold text-[#e50914] lg:text-xl">Please Enter any Amount!</div>:isloading? <div className="flex justify-center"><Loader/></div>:<div className="flex flex-col text-lg font-semibold gap-1">
+          <p className="text-center lg:text-xl">Converted Amount:</p> 
           <div>
-          <p className="text-center">{convertedAmount} {toCurrency}</p>
+          <p className="text-center lg:text-xl">{convertedAmount} {toCurrency}</p>
           </div>
           </div>}
       </section>
--- src/Components/ListCurrenceyName/ListofCurrenceyName.jsx
+++ src/Components/ListCurrenceyName/ListofCurrenceyName.jsx
@@ -3,7 +3,7 @@
 const CurrencyOptions = ({ currencies, currenciesdiraction, setDiraction }) => {
   return (
     <div>
-      <select value={currenciesdiraction} onChange={(e) => setDiraction(e.target.value)} className="border border-black py-1 px-1 text-lg font-semibold rounded cursor-pointer hover:shadow-md hover:shadow-teal-500 shadow-black shadow-md">
+      <select value={currenciesdiraction} onChange={(e) => setDiraction(e.target.value)} className="border border-black py-1 px-1 text-lg font-semibold rounded cursor-pointer hover:shadow-md hover:shadow-teal-500 shadow-black shadow-md lg:py-2 lg:px-3 lg:text-xl">
         {currencies.map((currency) => (
           <option key={currency} value={currency} className="cursor-pointer">
             {currency}
