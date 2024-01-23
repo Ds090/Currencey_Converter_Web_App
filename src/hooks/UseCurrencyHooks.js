522fd650973c3e76fe9930a1979bb052efb773d5
Divanshu Kachhawa <divanshukachawa7878@gmail.com>
2024-01-23 13:14:38 +0530

    Code Update

--- src/hooks/UseCurrencyHooks.js
+++ src/hooks/UseCurrencyHooks.js
@@ -5,67 +5,65 @@ import { useEffect, useState } from "react";
 
 function useHooksCurrencey() {
   const [currencies, setCurrencies] = useState([]);
-  const [fromCurrency, setFromCurrency] = useState('USD');
-  const [toCurrency, setToCurrency] = useState('INR');
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("INR");
   const [amount, setAmount] = useState(1);
   const [convertedAmount, setConvertedAmount] = useState(null);
   const [isloading, setisloading] = useState(true);
   const [fromFlage, setFromFlage] = useState(null);
   const [toFlage, settoFlage] = useState(null);
 
-  useEffect(() => {
-    const fetchCurrencies = async () => {
-      try {
-        const response = await axios.get('https://open.er-api.com/v6/latest');
-        const currencyList = Object.keys(response.data.rates);
-        console.log(currencyList);
-        setCurrencies(currencyList);
-      } catch (error) {
-        console.error('Error fetching currencies:', error);
-      }
-    };
-  
-    fetchCurrencies();
-  }, []);
-  
+  async function fetchCurrencies() {
+    try {
+      const response = await axios.get("https://open.er-api.com/v6/latest");
+      const currencyList = Object.keys(response.data.rates);
+      console.log(currencyList);
+      setCurrencies(currencyList);
+    } catch (error) {
+      console.error("Error fetching currencies:", error);
+    }
+  }
 
-  
-    const fetchCurrencyFlags = async () => {
-      try {
-        // Replace these URLs with the actual URLs of your flag images based on currency codes
-        const fromFlag = `https://flagcdn.com/48x36/${fromCurrency.toLowerCase().substring(0,2)}.png`;
-        const toFlag = `https://flagcdn.com/48x36/${toCurrency.toLowerCase().substring(0,2)}.png`;
-        // console.log(fromFlag);
+  async function fetchCurrencyFlags() {
+    try {
+      // Replace these URLs with the actual URLs of your flag images based on currency codes
+      const fromFlag = `https://flagcdn.com/48x36/${fromCurrency
+        .toLowerCase()
+        .substring(0, 2)}.png`;
+      const toFlag = `https://flagcdn.com/48x36/${toCurrency
+        .toLowerCase()
+        .substring(0, 2)}.png`;
+      // console.log(fromFlag);
 
-        setFromFlage(fromFlag);
-        settoFlage(toFlag);
-      } catch (error) {
-        console.error('Error fetching currency flags:', error);
-      }
-    };
-
-    
- 
+      setFromFlage(fromFlag);
+      settoFlage(toFlag);
+    } catch (error) {
+      console.error("Error fetching currency flags:", error);
+    }
+  }
 
   async function convertCurrency() {
     setisloading(true);
     setTimeout(async () => {
       try {
-        const response = await axios.get(`https://open.er-api.com/v6/latest/${fromCurrency}`);
+        const response = await axios.get(
+          `https://open.er-api.com/v6/latest/${fromCurrency}`
+        );
         const rate = response.data.rates[toCurrency];
+        console.log(rate);
         const result = (amount * rate).toFixed(2);
         setConvertedAmount(result);
       } catch (error) {
-        console.error('Error converting currency:', error);
+        console.error("Error converting currency:", error);
       }
-          setisloading(false);
+      setisloading(false);
     }, 2000);
   }
 
   useEffect(() => {
-        convertCurrency();
-        fetchCurrencyFlags();
-    
+    fetchCurrencies();
+    convertCurrency();
+    fetchCurrencyFlags();
   }, [fromCurrency, toCurrency, amount, fromCurrency, toCurrency]);
 
   const swapCurrency = () => {
@@ -87,8 +85,8 @@ function useHooksCurrencey() {
     convertedAmount,
     swapCurrency,
     isloading,
-    fromFlage, 
-    toFlage
+    fromFlage,
+    toFlage,
   };
 }
 
